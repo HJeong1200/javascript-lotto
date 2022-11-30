25c23cbcf40f97e2a13a6a95ef7e59643b2dd97d
HJeong1200 <hjeong1200@gmail.com>
2022-11-30 22:44:44 +0900

    feat: 입력한 구입 금액에 대하여 예외 확인하는 기능

--- src/Error.js
+++ src/Error.js
@@ -1,5 +1,14 @@
+const errorMessage = {
+  PURCHASEERROR: "[ERROR] 1,000원 단위 숫자를 입력해 주세요.",
+};
+
 const Errors = {
-  purchaseError() {},
+  purchaseError(input) {
+    const money = Number(input);
+    if (isNaN(money) || money % 1000) {
+      throw new Error(errorMessage.PURCHASEERROR);
+    }
+  },
 };
 
 module.exports = Errors;
